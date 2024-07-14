f51a279032ea7660518e52778982fc24266031c1
amiriar <amiriar.irr@gmail.com>
2024-07-14 10:19:25 +0330

    rthg

--- src/modules/students/students.controller.js
+++ src/modules/students/students.controller.js
@@ -34,11 +34,12 @@ class StudentController {
     try {
       const { pcId, username, password, course } = req.body;
 
-      if (!(course === "network") || !(course === "software")) {
+      if (course !== 'network' && course !== 'software') {
         return res.status(401).json({
           error: "لطفا فیلد هارا دست کاری نکنید!!.",
         });
       }
+
 
       if (!pcId || !username || !password || !course) {
         return res.status(401).json({
@@ -82,7 +83,7 @@ class StudentController {
 
   async logout(req, res, next) {
     try {
-      const result = this.#service.Logout()
+      const result = this.#service.Logout();
       return res.clearCookie(CookieNames.AccessToken).status(200).json({
         message: Authmessage.LoggedOutSuccessfully,
       });
@@ -93,12 +94,12 @@ class StudentController {
 
   async preventWhenLoggedIn(req, res, next) {
     if (req?.cookies.accessToken) {
-        return res.status(400).json({
-            message: "شما از قبل وارد شده اید."
-        });
+      return res.status(400).json({
+        message: "شما از قبل وارد شده اید.",
+      });
     }
     next();
-}
+  }
 }
 
 module.exports = new StudentController();
